35a26ad76e88f904d0f9be1059e483c9bbe2b403
zerogon <safoma@naver.com>
2024-10-05 18:43:08 +0900

    add secure file path and saveas update

--- xlsxTohwp/test.py
+++ xlsxTohwp/test.py
@@ -1,15 +1,19 @@
 import win32com.client as win32
 import pandas as pd
 
-filepath = r'C:\workspace\xlsxTohwp\data\output.hwp'
-outputpath = r'C:\workspace\xlsxTohwp\output\test.hwp'
+filepath = r'C:\workspace\solution\xlsxTohwp\data\inputTable.hwp'
+outputpath = r'C:\workspace\solution\xlsxTohwp\output\output.hwp'
 
-# 한글 파일 열기
-hwp = win32.Dispatch("HWPFrame.HwpObject")
+df = pd.read_excel('data/inputData.xlsx')  # 엑셀 파일 경로
+
+hwp = win32.Dispatch('HWPFrame.HwpObject')
+hwp.RegisterModule("FilePathCheckDLL", "FilePathCheckerModule")
 hwp.XHwpWindows.Item(0).Visible = True  # 한글 창 보이기
 hwp.Open(filepath,"HWP","") # 기존 HWP 파일 열기
+
 hwp.Run("TableCellBlock")
+hwp.Run("TableDeleteCell") 
 hwp.HAction.GetDefault("InsertText", hwp.HParameterSet.HInsertText.HSet)
-hwp.HParameterSet.HInsertText.Text = "test test test"
+hwp.HParameterSet.HInsertText.Text = "tes"
 hwp.HAction.Execute("InsertText", hwp.HParameterSet.HInsertText.HSet)
-hwp.SaveAs("test.hwp","HWP","download:true")
+hwp.SaveAs(outputpath,"HWP","")
